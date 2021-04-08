2b042771bdcc70b1cab476932e2dab8b33cd54b1
advya <50411324+Advya@users.noreply.github.com>
2021-04-08 17:11:09 +0300

    new controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'contacts/show'
+
+  resource :about, only: [:new, :create]  
   # resources :contacts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
